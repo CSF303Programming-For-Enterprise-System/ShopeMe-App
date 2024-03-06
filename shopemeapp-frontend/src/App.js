e1ebdc38f9dffaea671e8e0fa992429b282f4571
Sonam.Iscoding <sonam.iscoding@gmail.com>
2024-03-06 07:49:39 +0000

    Added the Admin Home Page Code

--- shopemeapp-frontend/src/App.js
+++ shopemeapp-frontend/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <div>
+            <h1>Welcome to Shopme Admin Control Panel</h1>
+            <div className="container-fluid">
+                <div>
+                    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+                        <a className="navbar-brand" href="/">
+                            <img src="/images/ShopmeAdminSmall.png" alt="Shopme Admin" />
+                        </a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#topNavbar">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="topNavbar">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/users">Users</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/categories">Categories</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/brands">Brands</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/products">Products</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/customers">Customers</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/shipping">Shipping</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/orders">Orders</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/report">Sales Report</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/articles">Articles</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/menus">Menus</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/settings">Settings</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+                <div>
+                    <h1>Shopme Control Panel</h1>
+                </div>
+                <div>
+                    <p>Shopme Control Panel - Copyright &copy; Shopme</p>
+                </div>
+            </div>
+        </div>
+);
+};
+
 
 export default App;
